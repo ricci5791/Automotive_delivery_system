0811881bee7978d4d8604fb5322f8df7a41ba162
ricci5791 <htcone6759@gmail.com>
2021-03-20 14:23:32 +0200

    basic list views

--- backend_api/views.py
+++ backend_api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, views, status
+from rest_framework.response import Response
 
-# Create your views here.
+from backend_api.databaseadapter import DatabaseAdapter
+from backend_api import serializers
+from backend_api.pathfinder import Path
+
+
+class DroneListView(generics.ListAPIView):
+    queryset = DatabaseAdapter.get_drones_list()
+    serializer_class = serializers.DroneSerializer
+
+
+class OwnerListView(generics.ListAPIView):
+    queryset = DatabaseAdapter.get_owners_list()
+    serializer_class = serializers.OwnerSerializer
+
+
+class RecipientListView(generics.ListAPIView):
+    queryset = DatabaseAdapter.get_recipients_list()
+    serializer_class = serializers.RecipientSerializer
+
+
+class OrderListView(generics.ListAPIView):
+    queryset = DatabaseAdapter.get_orders_list()
+    serializer_class = serializers.OrderSerializer
