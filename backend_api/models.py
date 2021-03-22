187451b12b0093fdfc33d9a18522e43fda7de5eb
ricci5791 <htcone6759@gmail.com>
2021-03-22 15:01:54 +0200

    changed structure of storing location logic

--- backend_api/models.py
+++ backend_api/models.py
@@ -26,7 +26,8 @@ class Drone(models.Model):
     id = models.IntegerField(primary_key=True)
     status = models.CharField(max_length=20, choices=DRONE_STATUS_LIST, default=IDLE)
     lift_capacity = models.IntegerField()
-    location = models.FloatField()
+    location_x = models.FloatField()
+    location_y = models.FloatField()
     charge = models.FloatField()
     owner = models.ForeignKey(Owner, on_delete=models.DO_NOTHING)
 
@@ -38,11 +39,11 @@ class Recipient(models.Model):
 
 
 class Order(models.Model):
-    INFO_RECEIVED = "Information received"
-    IN_TRANSIT = "In transit"
-    OUT_OF_DELIVERY = "Out for delivery"
-    AVAILABLE_FOR_PICKUP = "Available for pickup"
-    DELIVERED = "Delivered"
+    INFO_RECEIVED = "REC"
+    IN_TRANSIT = "TRN"
+    OUT_OF_DELIVERY = "ODV"
+    AVAILABLE_FOR_PICKUP = "APC"
+    DELIVERED = "DLV"
 
     ORDER_STATUS_LIST = [
         (INFO_RECEIVED, "Information received"),
@@ -55,8 +56,10 @@ class Order(models.Model):
     id = models.IntegerField(primary_key=True)
     mass = models.FloatField()
     status = models.CharField(max_length=20, choices=ORDER_STATUS_LIST, default=INFO_RECEIVED)
-    departure_point = models.CharField(max_length=30)
-    arrival_point = models.CharField(max_length=30)
+    departure_point_x = models.FloatField()
+    departure_point_y = models.FloatField()
+    arrival_point_x = models.FloatField()
+    arrival_point_y = models.FloatField()
     aprox_arrival_time = models.DateTimeField()
     distance = models.FloatField()
     recipient_token = models.ForeignKey(Recipient, on_delete=models.CASCADE)
